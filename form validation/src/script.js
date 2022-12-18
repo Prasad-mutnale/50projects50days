6340093ae4e8e333a492889baf9eac766216a7e5
PrasadMutnale-exe <prasadmutnale1234@gmail.com>
2022-12-19 01:06:26 +0530

    code changed

--- form validation/src/script.js
+++ form validation/src/script.js
@@ -3,74 +3,88 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
-const btn = document.getElementById('btn')
+const btn = document.getElementById('btn');
 
-form.addEventListener('submit', e=>{
+form.addEventListener('submit', e => {
     e.preventDefault();
-    validateInput();
-    display();
-})
 
-const setError = (element,message)=>{
-    const inputcontrol = element.parentElement;
-    const errorDisplay = inputcontrol.querySelector('.error');
+    validateInputs();
+    
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
     errorDisplay.innerText = message;
-    inputcontrol.classList.add('error');
-    inputcontrol.classList.remove('success');
-}
-const setSuccess = element =>{
-    const inputcontrol = element.parentElement;
-    const successDisplay = inputcontrol.querySelector('.error');
-    successDisplay.innerText = '';
-    inputcontrol.classList.add('success');
-    inputcontrol.classList.remove('error');
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
 }
 
-const validateInput = ()=>{
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+    
+};
+
+// const isValidEmail = email => {
+//     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(String(email).toLowerCase());
+// }
+
+const validateInputs = () => {
     const usernameValue = username.value.trim();
-    const emailValue = email.value.trim(); 
+    const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
 
-    if(usernameValue ===""){
-            setError(username,"Username is required");
-    }else{
-        setSuccess(username)
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
     }
 
-
-    if(emailValue === ''){
-        setError(email,'Email is required');
-    }else{
-        setSuccess(email)
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } 
+    else {
+        setSuccess(email);
     }
 
-    if(passwordValue === ""){
-        setError(password,"password is required");
-    }else if(passwordValue.length < 8){
-        setError(password ,"Password must be at least 8 character");
-    }
-    else{
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
         setSuccess(password);
     }
 
-
-    if(password2Value === ""){
-        setError(password2,"Please Confirm your password");
-    }else if(password2Value !== passwordValue){
-        setError(password,"Password does not match")
-    } else{
-        setSuccess(password);
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
     }
-
-
-  
-};
-
-const display=()=>{
-    document.write("<h2> "+username.value+ "</h2>");
+    btn.addEventListener("click", function(){
+        document.write("<h2> "+username.value+ "</h2>");
     document.write("<h2>"+email.value+"</h2>");
     document.write("<h2>"+password.value+ "</h2>");
-}
+    });
+    
+
+};
+
+
+
+// function display(){
+//     document.write("<h2> "+username.value+ "</h2>");
+//     document.write("<h2>"+email.value+"</h2>");
+//     document.write("<h2>"+password.value+ "</h2>");
+// };
 
 
